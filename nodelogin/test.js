9791989629f9663438f3aff7a4084df57fd73e58
ameynawale <32404062+ameynawale@users.noreply.github.com>
2017-10-15 20:53:30 -0700

    Password Encryption and Mocha tests
    This change contains the password encryption and some more mocha tests for the server side API testing.

--- nodelogin/test.js
+++ nodelogin/test.js
@@ -9,52 +9,112 @@ var request = require('supertest');
 var server = require('./bin/www');
 
 const userCredentials = {
-    username: 'f@l.com',
+    username: 'first1@last1.com',
     password: 'password'
 }
 
 var authenticatedUser = request.agent(server);
 
-describe('Server test', function(){
+describe('Server test', function() {
 
     /*before(function(){
         server.listen(3001);
     })*/
 
-    describe('Log in', function(){
-        it('should log in the user successfully', function(done){
-
-            authenticatedUser
-                .post('/users/doLogin')
-                .send(userCredentials)
-                .end(function(err, response){
-                    expect(response.statusCode).to.equal(201);
-                    expect('Location','/home');
-                    done();
-                });
-
-
-        });
-    });
-
-    describe('Sign up', function(){
-        it('should register the user successfully', function(done){
+    describe('Get User Files', function () {
+        it('Get User files successfully', function (done) {
             const newUser = {
-                firstname: 'first',
-                lastname: 'last',
-                email: 'first@last.com',
-                password: 'password'
+                username: 'first1@last1.com'
             }
             authenticatedUser
-                .post('/users/doSignup')
+                .post('/users/files')
                 .send(newUser)
-                .end(function(err, response){
+                .end(function (err, response) {
+                    if(err){
+                        console.log('error');
+                    }
                     expect(response.statusCode).to.equal(201);
-                    expect('Location','/login');
+                    expect('Location', '/home');
                     done();
                 });
 
 
         });
-    })
+
+        describe('Log in', function () {
+            it('Should log in the user successfully', function (done) {
+
+                authenticatedUser
+                    .post('/users/doLogin')
+                    .send(userCredentials)
+                    .end(function (err, response) {
+                        expect(response.statusCode).to.equal(201);
+                        expect('Location', '/home');
+                        done();
+                    });
+
+
+            });
+        });
+
+        describe('Sign up', function () {
+            it('Should register the user successfully', function (done) {
+                const newUser = {
+                    firstname: 'first',
+                    lastname: 'last',
+                    email: 'first@last.com',
+                    password: 'password'
+                }
+                authenticatedUser
+                    .post('/users/doSignup')
+                    .send(newUser)
+                    .end(function (err, response) {
+                        expect(response.statusCode).to.equal(201);
+                        expect('Location', '/login');
+                        done();
+                    });
+
+
+            });
+        });
+
+        describe('Share feature', function () {
+            it('Files should be shared successfully', function (done) {
+                const newUser = {
+                    emails: 'first1@last1.com',
+                    username: 'first2@last2.com',
+                    activeItemName: 'shared file.txt'
+                }
+                authenticatedUser
+                    .post('/users/doShare')
+                    .send(newUser)
+                    .end(function (err, response) {
+                        expect(response.statusCode).to.equal(201);
+                        expect('Location', '/Welcome');
+                        done();
+                    });
+
+
+            });
+        });
+        describe('File Download', function () {
+            it('Files should be downloaded successfully', function (done) {
+                const newUser = {
+                    emails: 'first1@last1.com',
+                    username: 'first2@last2.com',
+                    activeItemName: 'shared file.txt'
+                }
+                authenticatedUser
+                    .post('http://localhost:3001/files/download/first2@last2.com/shared file.txt')
+                    .send(newUser)
+                    .end(function (err, response) {
+                        //expect(response.statusCode).to.equal(201);
+                        expect('Location', '/Welcome');
+                        done();
+                    });
+
+
+            });
+        });
+    });
 })
